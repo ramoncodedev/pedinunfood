f2bab1b3a5f4b90e9f96e0dd666ff32ba0c6487d
ramoncodedev <ramonwebdesigner2025@gmail.com>
2026-04-26 20:22:19 -0300

    Primeiro commit - projeto Spring Boot

---
+++ src/main/java/com/pedinun/pedinunfood/PedinunfoodApplication.java
@@ -0,0 +1,13 @@
+package com.pedinun.pedinunfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedinunfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedinunfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedinun/pedinunfood/PedinunfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedinun.pedinunfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedinunfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
